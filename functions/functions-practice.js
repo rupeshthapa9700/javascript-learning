9f4d8085ae2515dd619e2c0445e5dd03f565015b
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-14 21:41:11 +0545

    learn: practice JavaScript functions with basic utilities

--- functions/functions-practice.js
+++ functions/functions-practice.js
@@ -54,3 +54,30 @@ function zeroDivision(num1, num2){
 console.log(zeroDivision(10, 0));
 
 
+function maskPassword(password){
+  return "*".repeat(password.length);
+}
+
+console.log(maskPassword("mypassword123"));
+
+function checkEmail(email){
+  if(email.includes("@") && email.includes(".")){
+    return "Valid Email";
+  } 
+  else {
+    return "Invalid Email";
+  }
+}
+console.log(checkEmail("rupesh@gmail.com"));
+
+function getUsername(username){
+ return username.slice(0, username.indexOf("@"));
+}
+
+console.log(getUsername("rupesh@gmail.com"));
+
+function countWords(words){
+  return words.split(" ").length;
+  
+}
+console.log(countWords("JavaScript is fun"));
