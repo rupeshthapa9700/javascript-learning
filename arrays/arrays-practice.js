6223453ae8f0ad8ac706568f63794008a4784ec7
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-17 10:30:00 +0545

    Add firstAndLast array utility function

--- arrays/arrays-practice.js
+++ arrays/arrays-practice.js
@@ -13,3 +13,8 @@ console.log(fruits);
 console.log(fruits.length);
 console.log(fruits.includes("Banana"));
 console.log(fruits.indexOf("Apple"));
+
+function firstAndLast(arr){
+  return `First: ${arr[0]} Last: ${arr[arr.length - 1]}`;
+}
+console.log(firstAndLast(["Apple", "Banana", "Mango"]));
