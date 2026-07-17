60ef45ef46d3bc0cee835e047b12e80fad3e7083
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-17 21:43:30 +0545

    Started array

---
+++ arrays/arrays-practice.js
@@ -0,0 +1,15 @@
+let fruits = ["Apple", "Banana"];
+
+fruits.push("Orange");
+fruits.unshift("Mango");
+
+console.log(fruits);
+
+fruits.pop();
+fruits.shift();
+
+console.log(fruits);
+
+console.log(fruits.length);
+console.log(fruits.includes("Banana"));
+console.log(fruits.indexOf("Apple"));
