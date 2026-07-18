6f29538fcf472cf38af332a38f416e22f8a8166b
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-18 11:15:00 +0545

    Add middleItem array utility function

--- arrays/arrays-practice.js
+++ arrays/arrays-practice.js
@@ -18,3 +18,10 @@ function firstAndLast(arr){
   return `First: ${arr[0]} Last: ${arr[arr.length - 1]}`;
 }
 console.log(firstAndLast(["Apple", "Banana", "Mango"]));
+
+function middleItem(lang){
+  let middle = Math.floor((lang.length - 1) / 2);
+  
+  return lang[middle];
+}
+console.log(middleItem(["HTML","CSS","JavaScript"]));
