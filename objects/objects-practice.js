8c31b04a96e372808fb4d4ef41d1fa00dc5b2b0e
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-08-04 18:02:29 +0545

    Car Manager

--- objects/objects-practice.js
+++ objects/objects-practice.js
@@ -28,3 +28,40 @@ const showStudent = (student) => {
   return student;
 }
 console.log(showStudent(student));
+
+
+
+const car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2020
+};
+
+const updateYear = (car, year) => {
+  car.year = year;
+  return car;
+}
+console.log(updateYear(car, 2026));
+
+const changeBrand = (car, brand) =>{
+  car.brand = brand;
+  return car;
+}
+console.log(changeBrand(car, 'Hyundai'));
+
+const deleteModel = (car) => {
+  delete car.model;
+  return car;
+}
+console.log(deleteModel(car));
+
+const hasModel = (car) => {
+  return car.hasOwnProperty('model');
+}
+console.log(hasModel(car));
+
+const showCar = (car) => {
+  return car;
+}
+console.log(showCar(car));
+
